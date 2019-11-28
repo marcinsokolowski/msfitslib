f973616a951ea23337038ed3a1d4fa07574bf131
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-11-28 17:02:11 +0800

    checking existance of RE and IM files

--- bg_vis.cpp
+++ bg_vis.cpp
@@ -1,4 +1,5 @@
 #include "bg_vis.h"
+#include <myfile.h>
 
 CBgVis::CBgVis( const char* basename, const char* postfix )
 : m_basename( basename ), m_postfix( postfix )
@@ -16,12 +17,22 @@ int CBgVis::Read( const char* basename , const char* postfix )
    char szRealName[1024],szImagName[1024];
    
    sprintf(szRealName,"%s_%s_RE.fits",m_basename.c_str(),m_postfix.c_str());
+   sprintf(szImagName,"%s_%s_IM.fits",m_basename.c_str(),m_postfix.c_str());
+   
+   if( !MyFile::DoesFileExist( szRealName ) ){
+      printf("WARNING : file %s does not exist !\n",szRealName);
+      return -1;
+   }
+   if( !MyFile::DoesFileExist( szImagName ) ){
+      printf("WARNING : file %s does not exist !\n",szImagName);
+      return -1;
+   }
+   
    if( m_real.ReadFits( szRealName ) ){
       printf("ERROR : could not read FITS file %s\n",szRealName);      
       return ret;
    }
 
-   sprintf(szImagName,"%s_%s_IM.fits",m_basename.c_str(),m_postfix.c_str());
    if( m_imag.ReadFits( szImagName ) ){
       printf("ERROR : could not read FITS file %s\n",szImagName);      
       return ret;
