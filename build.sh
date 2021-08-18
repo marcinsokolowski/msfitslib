6a59665bcb96e5ea9b2eb884af79403a16bf5fce
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-08-18 11:16:31 +0800

    build.sh updated with avg_images and modulefile for Topaz added

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-module load fftw/3.3.8
+module load fftw/3.3.8 
+module load libnova/0.15.0
 
 mkdir -p build/ lib/ bin/
 cd build
@@ -13,6 +14,7 @@ cp ux2sid ../bin/
 cp ux2sid_file ../bin/
 cp sid2ux ../bin/
 cp radec2azh ../bin/
+cp avg_images ../bin/
 
 # cp libmscommonlib.so* ../lib/
 
