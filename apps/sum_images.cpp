7a38b68bcb2f991ba19497f5eaf36912c0a7a1ca
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-03-07 15:28:28 +0800

    sum_images added

---
+++ apps/sum_images.cpp
@@ -0,0 +1,189 @@
+// program averages few FITS images of the same sizes 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
+
+#include <bg_globals.h>
+#include "bg_fits.h"
+#include <mystring.h>
+
+#include <vector>
+using namespace std;
+
+string list="fits_list";
+string out_fits="out.fits";
+string out_rms_fits="out_rms.fits";
+string beam_fits_file;
+
+double gMinRMSOnSingle  = 0.00001;
+double gMaxRMSOnSingle  = 1e20; // default was 4.00, 2023-12-22 -> changed to ~INFINITY to avoid skipping noisy images ; // maximum allowed RMS on 
+
+int gCalcMax=0;
+
+// WINDOW :
+int gBorderStartX=-1;
+int gBorderStartY=-1;
+int gBorderEndX=-1;
+int gBorderEndY=-1;
+int gCenterRadius = -1;
+bool gCalcRMSAroundPixel = false;
+bool gIgnoreMissingFITS = false;
+int gStartFitsIndex = 0;
+int gEndFitsIndex   = 1000000;
+
+void usage()
+{
+   printf("sum_images fits_list sum.fits\n\n\n");
+   exit(0);
+}
+
+void parse_cmdline(int argc, char * argv[]) {
+   char optstring[] = "hixr:w:c:C:S:E:B:";
+   int opt;
+        
+   while ((opt = getopt(argc, argv, optstring)) != -1) {
+//      printf("opt = %c (%s)\n",opt,optarg);   
+      switch (opt) {
+         case 'h':
+            // antenna1 = atol( optarg );
+            usage();
+            break;
+
+/*         case 'B':
+            beam_fits_file = optarg;
+            break;
+*/
+
+         default:   
+            fprintf(stderr,"Unknown option %c\n",opt);
+            usage();
+      }
+   }
+}
+
+void print_parameters()
+{
+    printf("############################################################################################\n");
+    printf("PARAMETERS :\n");
+    printf("############################################################################################\n");
+    printf("List file    = %s\n",list.c_str());
+    printf("Fits files range : %d - %d\n",gStartFitsIndex,gEndFitsIndex);
+    printf("out_fits     = %s\n",out_fits.c_str());
+    printf("############################################################################################\n");
+}
+
+int main(int argc,char* argv[])
+{
+  string list="fits_list";
+  if( argc >= 2 ){
+     list = argv[1];
+  }
+  out_fits="sum.fits";
+  if( argc >= 3 ){
+     out_fits = argv[2];
+  }  
+  out_rms_fits="out_rms.fits";
+  if( argc >= 4 ){
+     out_rms_fits = argv[3];
+  }
+  
+  parse_cmdline( argc , argv );
+  print_parameters();
+ 
+  
+  vector<string> fits_list;
+  if( bg_read_list(list.c_str(),fits_list) <= 0 ){
+     printf("ERROR : could not read list file %s\n",list.c_str());
+     exit(-1);
+  }else{
+     for(int i=0;i<fits_list.size();i++){
+        printf("%i %s\n",i,fits_list[i].c_str());
+     }
+  }
+  
+  //  int ReadFits( const char* fits_file=NULL, int bAutoDetect=0, int bReadImage=1, int bIgnoreHeaderErrors=0, bool transposed=false );
+  printf("Reading fits file %s ...\n",fits_list[gStartFitsIndex].c_str());
+  CBgFits first_fits, first_fits2;
+  if( first_fits.ReadFits( fits_list[gStartFitsIndex].c_str()  , 0, 1, 1 ) ){
+     printf("ERROR : could not read first fits file %s on the list\n",fits_list[gStartFitsIndex].c_str());
+     exit(-1); 
+  }else{
+     printf("OK : fits file %s read ok\n",fits_list[gStartFitsIndex].c_str());
+  }
+  
+  // read the same second time to save RMS 
+  if( strlen(out_rms_fits.c_str()) > 0 ){
+      if( first_fits2.ReadFits( fits_list[gStartFitsIndex].c_str() , 0, 1, 1 ) ){
+         printf("ERROR : could not read first fits file %s on the list\n",fits_list[gStartFitsIndex].c_str());
+         exit(-1); 
+      }else{
+         printf("OK : fits file %s read ok\n",fits_list[gStartFitsIndex].c_str());
+      }
+  }
+  
+  int size = first_fits.GetXSize()*first_fits.GetYSize();
+  
+  double* sum_tab = new double[size];
+  double* sum2_tab = NULL;
+  if( strlen(out_rms_fits.c_str()) > 0 ){
+      sum2_tab = new double[size];
+      printf("DEBUG : sum2_tab initialised\n");      
+  }
+
+  CBgFits* pBeamImage = NULL;
+  double* sum_beam = NULL;
+
+// 2021-11-06 : start with zeros to do everything in the loop (required after adding beam weighting) 
+  // initialise sum_tab and sum2_tab with values from the 1st image :
+//  float* first_fits_data_ptr = first_fits.get_data();
+  for (int pos=0;pos<size;pos++){
+//     double val = first_fits_data_ptr[pos];
+  
+     sum_tab[pos] = 0; // val;
+     if( sum2_tab ){
+         sum2_tab[pos] = 0; // val*val;
+     }
+  }
+  int good_image_count = 1; // first image already included 
+  
+  CBgFits fits;
+  // 2014-08-20 was from 1 but I've changed to 0 to average all the files 
+  int last_fits = fits_list.size();
+  if( gEndFitsIndex < last_fits ){
+     last_fits = gEndFitsIndex;
+  }
+  for(int i=(gStartFitsIndex);i<last_fits;i++){ // 2019-07-11 - start from the 2nd (1st or 0-based) image 
+     if( fits.ReadFits( fits_list[i].c_str() , 0, 1, 1 ) ){
+        if( gIgnoreMissingFITS ){
+           printf("WARNING : could not read fits file %s on the list, -i option means that it is ignored -> FITS file skipped\n",fits_list[i].c_str());
+           continue;
+        }else{
+           printf("ERROR : could not read fits file %s on the list\n",fits_list[i].c_str());
+           exit(-1); 
+        }
+     }else{
+        printf("OK : fits file %s read ok\n",fits_list[i].c_str());
+     }
+     
+     for (int pos=0;pos<size;pos++){
+        double val = fits.get_data()[pos];
+        sum_tab[pos] += val;
+     }
+  }
+
+  if( first_fits.WriteFits( out_fits.c_str() ) ){
+     printf("ERROR : could not write average fits file %s\n",out_fits.c_str());
+     exit(-1);
+  }  
+  printf("OK : output fits file %s written ok\n",out_fits.c_str());
+  printf("INFO : mean/rms calculated based on %d good images\n",good_image_count);
+
+  delete [] sum_tab;
+  if( sum2_tab ){
+      delete [] sum2_tab;
+  }
+  
+}
+
